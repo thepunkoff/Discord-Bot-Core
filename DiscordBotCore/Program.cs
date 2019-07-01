91a24f8653828ffdf86e7806c0a7a3ae71dc4406
thepunkoff <thepunkoff@gmail.com>
2019-07-01 23:31:52 +0300

    Add UnityContainer
    Implementer basic UnityContainer and registered IDataStorage

--- DiscordBotCore/Program.cs
+++ DiscordBotCore/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiscordBotCore.Storage;
+using DiscordBotCore.Storage.Implementations;
+using System;
 
 namespace DiscordBotCore
 {
@@ -6,7 +8,30 @@ namespace DiscordBotCore
     {
         private static void Main()
         {
+            Unity.RegisterTypes();
+
+            var a = Unity.Resolve<MyProfile>();
+            var b = Unity.Resolve<MyProfile>();
+
+
             Console.WriteLine("Hello, Discord");
+        }
+    }
+
+    public class MyProfile
+    {
+
+        private readonly IDataStorage _storage;
+
+        public MyProfile(IDataStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public void NewUser(string name)
+        {
+            var regTime = DateTime.UtcNow;
+            _storage.StoreObject(regTime, name);
         }
     }
 }
